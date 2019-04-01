f552a6f94ebb6cfd6224bc5aba6537f201a291fe
Simon Shillaker <mail@simonshillaker.com>
2019-04-01 16:23:18 +0100

    Back to normal getenv/setenv

--- src/env/getenv.c
+++ src/env/getenv.c
@@ -6,6 +6,10 @@ char *__strchrnul(const char *, int);
 
 char *getenv(const char *name)
 {
-	// Hack to hook into environment
-	return _getenv(name);
+    size_t l = __strchrnul(name, '=') - name;
+    if (l && !name[l] && __environ)
+        for (char **e = __environ; *e; e++)
+            if (!strncmp(name, *e, l) && l[*e] == '=')
+                return *e + l+1;
+    return 0;
 }

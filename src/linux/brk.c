58f99061381ff0c479bbe328837fe2f6a3afe043
Simon Shillaker <shillaker@gmail.com>
2019-05-25 13:52:10 +0100

    Added back brk and sbrk

--- src/linux/brk.c
+++ src/linux/brk.c
@@ -1,7 +1,17 @@
 #include <errno.h>
 #include "syscall.h"
 
+/*
+ * brk has been removed in the latest musl, however, it works well in a wasm scenario
+ * so we'll keep it.
+ */
 int brk(void *end)
 {
-	return __syscall_ret(-ENOMEM);
+    void *res = (void*) syscall(SYS_brk, end);
+
+    if(res == (void*) -1) {
+        return -1;
+    }
+
+    return 0;
 }
--- src/linux/sbrk.c
+++ src/linux/sbrk.c
@@ -2,8 +2,13 @@
 #include <errno.h>
 #include "syscall.h"
 
+/*
+ * As with brk, sbrk has been removed in current msul, however, it's useful in a wasm context.
+ * The original musl implementation made two calls to brk, whereas we can just all directly to the
+ * relevant intrinsic
+ */
 void *sbrk(intptr_t inc)
 {
-	if (inc) return (void *)__syscall_ret(-ENOMEM);
-	return (void *)__syscall(SYS_brk, 0);
+    void *res = (void*) syscall(SYS_sbrk, inc);
+    return res;
 }

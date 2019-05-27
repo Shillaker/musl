d7356ed737caf9b77828c42284e67138800b6057
Simon Shillaker <shillaker@gmail.com>
2019-05-27 17:24:14 +0100

    Hopefully fixed issue with opendir and annotated readdir

--- src/dirent/opendir.c
+++ src/dirent/opendir.c
@@ -12,10 +12,20 @@ DIR *opendir(const char *name)
 
 	if ((fd = open(name, O_RDONLY|O_DIRECTORY|O_CLOEXEC)) < 0)
 		return 0;
+
 	if (!(dir = calloc(1, sizeof *dir))) {
 		__syscall(SYS_close, fd);
 		return 0;
 	}
+
+	// 27/05/2019
+	// For readdir and other functions to work, the buf_pos and buf_end of the returned DIR
+	// must be zero. This should get done by calloc (which ought to return zeroed memory), but
+	// a bug in calloc (which still exists) means that occasionally the memory from calloc
+	// *isn't* zeroed, hence we set the variables manually here.
+	dir->buf_pos = 0;
+	dir->buf_end = 0;
+
 	dir->fd = fd;
 	return dir;
 }
--- src/dirent/readdir.c
+++ src/dirent/readdir.c
@@ -9,19 +9,54 @@ int __getdents(int, struct dirent *, size_t);
 struct dirent *readdir(DIR *dir)
 {
 	struct dirent *de;
-	
+
+
+	/*
+	 * This function is a little hard to understand and can be fragile.
+	 * Each call to readdir will return a single dirent, but a call to getdents
+	 * will fill up the whole buffer with multiple dirents.
+	 *
+	 * It then uses the DIR passed into the function to track how far it has moved
+	 * through the buffer (and whether it needs to call getdents again). The fields
+	 * are as follows:
+	 *
+	 * - dir->buf = pointer to the buffer (zero terminated, 2048 bytes long (hard-coded in __dirent.h))
+	 * - dir->buf_end = how many bytes we've read into this buffer (zero to begin with)
+	 * - dir->buf_pos = how much of the buffer we've read
+	 *
+	 * Clearly this depends heavily on the DIR not being modified between calls, and
+	 * the DIR being passed in properly zeroed on the first call.
+	 *
+	 * 27/05/2019 - there is a bug in calloc where the new memory isn't always zeroed properly
+	 * this meant opendir created a non-zero DIR, which caused this function to perform an
+	 * infinite loop.
+	 */
 	if (dir->buf_pos >= dir->buf_end) {
+	    // We enter this block on the first call, or if we've read through the whole buffer
+
+	    // Call getdents to populate the buffer with dirents
 		int len = __syscall(SYS_getdents, dir->fd, dir->buf, sizeof dir->buf);
+
+		// Drop out if there are none
 		if (len <= 0) {
 			if (len < 0 && len != -ENOENT) errno = -len;
 			return 0;
 		}
+
+		// Set up the buffer markers
 		dir->buf_end = len;
 		dir->buf_pos = 0;
 	}
+
+	// Get a pointer to the next dirent
 	de = (void *)(dir->buf + dir->buf_pos);
+
+	// Move the buffer position along to the beginning of the one after
 	dir->buf_pos += de->d_reclen;
+
+	// Not sure what this bit does.
 	dir->tell = de->d_off;
+
 	return de;
 }
 
